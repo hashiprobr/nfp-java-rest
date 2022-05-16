9893fa4e8901295e582469b01c37144a0978994e
Marcelo Hashimoto <marcelo.hashimoto@gmail.com>
2022-05-16 17:09:01 -0300

    Add support for returning 409

---
+++ src/main/java/br/pro/hashi/nfp/rest/server/exception/ConflictException.java
@@ -0,0 +1,11 @@
+package br.pro.hashi.nfp.rest.server.exception;
+
+import jakarta.servlet.http.HttpServletResponse;
+
+public class ConflictException extends ResponseException {
+	private static final long serialVersionUID = 4995831025823880390L;
+
+	public ConflictException(String message) {
+		super(HttpServletResponse.SC_CONFLICT, message);
+	}
+}
