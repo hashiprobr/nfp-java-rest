52d96e66fc97aaa7620f613859aef8250bdb2c08
Marcelo Hashimoto <marcelo.hashimoto@gmail.com>
2022-07-21 20:33:08 -0300

    Add final to converter methods

--- src/main/java/br/pro/hashi/nfp/rest/Converter.java
+++ src/main/java/br/pro/hashi/nfp/rest/Converter.java
@@ -22,7 +22,7 @@ public final class Converter {
 
 	private static abstract class ToNumber<J extends Number, T> extends AbstractConverter<J, T> {
 		@Override
-		protected JsonElement wrapNotNull(Number value, JsonSerializationContext context) {
+		protected final JsonElement wrapNotNull(Number value, JsonSerializationContext context) {
 			return new JsonPrimitive(value);
 		}
 	}
@@ -47,87 +47,87 @@ public final class Converter {
 
 	public static abstract class ToBoolean<T> extends AbstractConverter<Boolean, T> {
 		@Override
-		protected JsonElement wrapNotNull(Boolean value, JsonSerializationContext context) {
+		protected final JsonElement wrapNotNull(Boolean value, JsonSerializationContext context) {
 			return new JsonPrimitive(value);
 		}
 
 		@Override
-		protected Boolean unwrapNotNull(JsonElement value, JsonDeserializationContext context) {
+		protected final Boolean unwrapNotNull(JsonElement value, JsonDeserializationContext context) {
 			return value.getAsBoolean();
 		}
 	}
 
 	public static abstract class ToByte<T> extends ToNumber<Byte, T> {
 		@Override
-		protected Byte unwrapNotNull(JsonElement value, JsonDeserializationContext context) {
+		protected final Byte unwrapNotNull(JsonElement value, JsonDeserializationContext context) {
 			return value.getAsByte();
 		}
 	}
 
 	public static abstract class ToShort<T> extends ToNumber<Short, T> {
 		@Override
-		protected Short unwrapNotNull(JsonElement value, JsonDeserializationContext context) {
+		protected final Short unwrapNotNull(JsonElement value, JsonDeserializationContext context) {
 			return value.getAsShort();
 		}
 	}
 
 	public static abstract class ToInt<T> extends ToNumber<Integer, T> {
 		@Override
-		protected Integer unwrapNotNull(JsonElement value, JsonDeserializationContext context) {
+		protected final Integer unwrapNotNull(JsonElement value, JsonDeserializationContext context) {
 			return value.getAsInt();
 		}
 	}
 
 	public static abstract class ToLong<T> extends ToNumber<Long, T> {
 		@Override
-		protected Long unwrapNotNull(JsonElement value, JsonDeserializationContext context) {
+		protected final Long unwrapNotNull(JsonElement value, JsonDeserializationContext context) {
 			return value.getAsLong();
 		}
 	}
 
 	public static abstract class ToFloat<T> extends ToNumber<Float, T> {
 		@Override
-		protected Float unwrapNotNull(JsonElement value, JsonDeserializationContext context) {
+		protected final Float unwrapNotNull(JsonElement value, JsonDeserializationContext context) {
 			return value.getAsFloat();
 		}
 	}
 
 	public static abstract class ToDouble<T> extends ToNumber<Double, T> {
 		@Override
-		protected Double unwrapNotNull(JsonElement value, JsonDeserializationContext context) {
+		protected final Double unwrapNotNull(JsonElement value, JsonDeserializationContext context) {
 			return value.getAsDouble();
 		}
 	}
 
 	public static abstract class ToBigInteger<T> extends ToNumber<BigInteger, T> {
 		@Override
-		protected BigInteger unwrapNotNull(JsonElement value, JsonDeserializationContext context) {
+		protected final BigInteger unwrapNotNull(JsonElement value, JsonDeserializationContext context) {
 			return value.getAsBigInteger();
 		}
 	}
 
 	public static abstract class ToBigDecimal<T> extends ToNumber<BigDecimal, T> {
 		@Override
-		protected BigDecimal unwrapNotNull(JsonElement value, JsonDeserializationContext context) {
+		protected final BigDecimal unwrapNotNull(JsonElement value, JsonDeserializationContext context) {
 			return value.getAsBigDecimal();
 		}
 	}
 
 	public static abstract class ToString<T> extends AbstractConverter<String, T> {
 		@Override
-		protected JsonElement wrapNotNull(String value, JsonSerializationContext context) {
+		protected final JsonElement wrapNotNull(String value, JsonSerializationContext context) {
 			return new JsonPrimitive(value);
 		}
 
 		@Override
-		protected String unwrapNotNull(JsonElement value, JsonDeserializationContext context) {
+		protected final String unwrapNotNull(JsonElement value, JsonDeserializationContext context) {
 			return value.getAsString();
 		}
 	}
 
 	public static abstract class ToList<E, T> extends ToCollection<List<E>, T> {
 		@Override
-		protected JsonElement wrapNotNull(List<E> value, JsonSerializationContext context) {
+		protected final JsonElement wrapNotNull(List<E> value, JsonSerializationContext context) {
 			JsonArray array = new JsonArray();
 			for (E element : value) {
 				array.add(context.serialize(element));
@@ -136,7 +136,7 @@ public final class Converter {
 		}
 
 		@Override
-		protected List<E> unwrapNotNull(JsonElement value, JsonDeserializationContext context) {
+		protected final List<E> unwrapNotNull(JsonElement value, JsonDeserializationContext context) {
 			List<E> list = new ArrayList<>();
 			for (JsonElement element : value.getAsJsonArray()) {
 				list.add(context.deserialize(element, elementType));
@@ -147,7 +147,7 @@ public final class Converter {
 
 	public static abstract class ToMap<V, T> extends ToCollection<SortedMap<String, V>, T> {
 		@Override
-		protected JsonElement wrapNotNull(SortedMap<String, V> value, JsonSerializationContext context) {
+		protected final JsonElement wrapNotNull(SortedMap<String, V> value, JsonSerializationContext context) {
 			JsonObject object = new JsonObject();
 			for (String key : value.keySet()) {
 				object.add(key, context.serialize(value.get(key)));
@@ -156,7 +156,7 @@ public final class Converter {
 		}
 
 		@Override
-		protected SortedMap<String, V> unwrapNotNull(JsonElement value, JsonDeserializationContext context) {
+		protected final SortedMap<String, V> unwrapNotNull(JsonElement value, JsonDeserializationContext context) {
 			JsonObject object = value.getAsJsonObject();
 			SortedMap<String, V> map = new TreeMap<>();
 			for (String key : object.keySet()) {
